721510c496a11fb240d57896cb5f2fc61b7c0fe9
zemfira1 <craftyzema@gmail.com>
2023-07-24 11:19:04 -0400

    delete homework 3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -49,22 +49,6 @@ export class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== id),
     }))
   }
-
-/////////////////////////////////////////////// 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-//////////////////////////////////////////////////
   
   render() {
     const filter = this.state.filter;
